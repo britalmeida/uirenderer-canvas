ead52c8e1975ad50cc39fde1ba83be88538a6cd8
Francesco Siddi <francesco.siddi@gmail.com>
2022-09-10 00:44:44 +0200

    Switch to Javascript import/export statements

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const
-  Shading = require('./lib/shading'),
-  Layout = require('./lib/layout'),
-  Text = require('./lib/textbox');
-
-exports.Rect = Shading.Rect;
-exports.View = Shading.View;
-exports.UIRenderer = Shading.UIRenderer;
-exports.UILayout = Layout;
-exports.TextBox = Text.TextBox;
-exports.TextStyle = Text.TextStyle;
+export { Rect, View, UIRenderer } from './lib/shading';
+export { TextBox, TextStyle } from './lib/textbox';
+export * as UILayout from './lib/layout';
