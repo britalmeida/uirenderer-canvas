8eb4541908bcf531c2ea6a6d77d961bd7e326d1e
Ines Almeida <britalmeida@gmail.com>
2023-07-30 22:26:24 +0200

    Project configuration fixes

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export { vec2, vec4, Rect, View, UIRenderer } from './src/shading';
+export { Rect, View, UIRenderer } from './src/shading';
 export { TextBox, TextStyle } from './src/textbox';
 export * as UILayout from './src/layout';
